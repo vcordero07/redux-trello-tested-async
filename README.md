ba25a7ee71c862d3b30d3bd575f9b192ff5db9a5
vcordero07 <vcordero07@gmail.com>
2018-02-04 17:28:55 -0500

    wip

---
+++ README.md
@@ -0,0 +1,1 @@
+## Redux trello tested async
---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./card.css";
+
+export default function Card(props) {
+  return <div className="card">{props.text}</div>;
+}
+
+Card.defaultProps = {
+  text: ""
+};
